76a00330525446a4cde98f6c097d745e33a088ad
MarsTL <165991919+MarsTL@users.noreply.github.com>
2025-01-15 16:37:36 -0800

    hw1 completed

--- hw1/01-fizzbuzz.js
+++ hw1/01-fizzbuzz.js
@@ -6,8 +6,21 @@ Write a program that writes all the numbers from 1 to 100, with some exceptions:
 - For numbers divisible by 3 and 5, print “fizzbuzz”
 
 Use console.log() to write the proper output to the command line.
-
 **/
+
+//done with partner
+
+const fizzbuzz = function fizzbuzz(end){
+  for (let i = 1; i < end +1; i++){
+    result = ""
+    if (i % 3 === 0) result = "fizz";
+    if (i % 5 === 0) result = "buzz";
+    if (result === "") result = String(i);
+    console.log(result);
+  }
+};
+
+fizzbuzz(100);
 
 // 1
 // 2
