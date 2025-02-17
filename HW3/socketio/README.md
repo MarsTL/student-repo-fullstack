79a4e13d1b78f4a6e2b9acfb06352158b80b469a
MarsTL <165991919+MarsTL@users.noreply.github.com>
2025-02-16 17:13:12 -0800

    Re-added HW3 as a regular directory

---
+++ HW3/socketio/README.md
@@ -0,0 +1,10 @@
+# TypeScript Exercises
+
+## Setup
+1. Install TypeScript globally (if not installed):
+   ```sh
+   npm install -g typescript
+2. Install dependencies
+  npm install
+3. Run typeScript exercise 
+  tsc exercise1.ts && node exercise1.js
---
+++ HW3/socketio/index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  
+  // Emit a message when a new user connects
+  socket.emit('chat message', 'Welcome to the chat Benihanna!');
+  
+  // Listen for chat messages and emit them to all clients
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+    io.emit('chat message', msg); // Broadcast the message to all clients
+  });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
+
+// This will emit the event to all connected sockets
+io.emit('some event', { someProperty: 'some value', otherProperty: 'other value' }); 
+
+
+
